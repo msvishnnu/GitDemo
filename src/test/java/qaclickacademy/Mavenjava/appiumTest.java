04f1e2432f3f302f170a81b24981c1c67e483369
vishnu <msvishnu70@gmail.com>
2020-06-08 19:11:41 +0530

    first commit

---
+++ src/test/java/qaclickacademy/Mavenjava/appiumTest.java
@@ -0,0 +1,17 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class appiumTest {
+
+	@Test
+	public void NativeAppAndroid() {
+		System.out.println("NativeAppAndroid");
+	}
+
+	@Test
+	public void IOSApps() {
+		System.out.println("IOSApps");
+	}
+
+}
---
+++ src/test/java/qaclickacademy/Mavenjava/seleniumTest.java
@@ -0,0 +1,17 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class seleniumTest {
+
+	@Test
+	public void BrowserAutomation() {
+		System.out.println("BrowserAutomation");
+	}
+
+	@Test
+	public void elementsUi() {
+		System.out.println("elementsUi");
+	}
+
+}
